f286200abce665c35e78765bb2140bd5bd85348a
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-04-10 20:04:03 +0530

    4.6 executing sql commands

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -17,17 +17,31 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-// This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := `
+		INSERT INTO snippets (title, content, created, expires)
+		VALUES ($1, $2, NOW(), NOW() + ($3 || ' days')::interval)
+		RETURNING id
+	`
+
+	var id int
+	err := m.DB.QueryRow(stmt, title, content, expires).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
-// This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (Snippet, error) {
+	stmt := `SELECT title, content, created, expires FROM snippets WHERE 
+	expires > NOW() AND id = $1`
+	err := m.DB.QueryRow(stmt, id)
+
+	var s Snippet
 	return Snippet{}, nil
 }
 
-// This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]Snippet, error) {
 	return nil, nil
 }
